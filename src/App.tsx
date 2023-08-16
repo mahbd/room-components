0df16764d753994160eaff08d1852c5403f6ede9
Mahmudul Alam <mahmudula2000@gmail.com>
2023-08-16 09:25:36 +0600

    added firebase authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import Item from "./components/Item";
 import NavBar from "./components/NavBar";
 import {useEffect} from "react";
 import useItemStore from "./store/ItemStore";
+import {initializeApp} from "firebase/app";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCO0uh8b343HpOCZFidlQVbXaRkvkrUzhU",
+    authDomain: "room-components-c66b8.firebaseapp.com",
+    databaseURL: "https://room-components-c66b8-default-rtdb.firebaseio.com",
+    projectId: "room-components-c66b8",
+    storageBucket: "room-components-c66b8.appspot.com",
+    messagingSenderId: "890750296374",
+    appId: "1:890750296374:web:2919ef09f1111dbc8b5397"
+};
 
 function App() {
     useEffect(() => {
@@ -10,9 +20,11 @@ function App() {
         useItemStore.getState().loadData();
     }, []);
 
+    const app = initializeApp(firebaseConfig);
+
     return (
         <div>
-            <NavBar/>
+            <NavBar app={app}/>
             <Item key={1} itemId={1}/>
         </div>
     )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,40 @@
-import {Heading, HStack} from "@chakra-ui/react";
+import {Button, Heading, HStack} from "@chakra-ui/react";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {useAuthState} from "react-firebase-hooks/auth";
 import SearchInput from "./SearchInput";
 
+interface Props {
+    app: any
+}
 
-const NavBar = () => {
+const NavBar = ({app}: Props) => {
+    const auth = getAuth(app);
+    const [user] = useAuthState(auth);
+    const googleProvider = new GoogleAuthProvider();
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (err: any) {
+            console.error(err);
+            alert(err.message);
+        }
+    };
+
+    const logout = () => {
+        auth.signOut();
+    }
+
     return (
         <div>
             <HStack justifyContent={'space-between'}>
                 <Heading as={'h1'} size={'lg'}>Components</Heading>
-                <SearchInput/>
+                <HStack>
+                    <SearchInput/>
+                    {user && <Button marginRight={2} onClick={logout}>Logout</Button>}
+                    {!user && <Button marginRight={2} onClick={signInWithGoogle}>
+                        Login/Sign Up
+                    </Button>}
+                </HStack>
             </HStack>
         </div>
     );
